010aeba31f277f72738adedcbbb18bfdb5191470
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-05-15 15:37:40 +0700

    menambahkan void addNode  dengan integer nim karna belum memiliki nilai

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -24,4 +24,11 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
 };
