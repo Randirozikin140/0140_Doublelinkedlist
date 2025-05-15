f4a8481ba708b0069a1eec0f0e792acb04a924dd
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-05-15 18:09:38 +0700

    while dalam step 8

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -56,7 +56,16 @@ public:
         //insert to between node
         //step 8: Locate position for insertion
         Node *current = START;
-        
+        while((current->next != NULL && current->next->noMhs < nim))
+        {
+            current = current->next;
+        }
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate number not allowed" << endl;
+            return;
+        }
 
+        
     }
 };
